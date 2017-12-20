4d703f41cf11556422c59ef28631c06f99c1c9b7
sombrastudios <nils.martel@concepts-system.com>
2017-12-20 19:36:57 +0100

    Implement Display Trait for F2

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 use std::ops::Add;
+use std::fmt;
 
 #[cfg(test)]
 mod tests {
@@ -38,5 +39,14 @@ impl Add for F2 {
             return F2::False;
         }
         F2::True
+    }
+}
+
+impl fmt::Display for F2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &F2::True => write!(f, "True"),
+            &F2::False => write!(f, "False"),
+        }
     }
 }
